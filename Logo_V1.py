58f7a7c30c8b91caf374afd7019b7acd2b896082
Nathan Kim <rockwall255@gmail.com>
2015-01-12 14:00:25 -0600

    Logo V1.2
    A little changes to the code. added comments

--- Logo_V1.py
+++ Logo_V1.py
@@ -4,11 +4,13 @@ import PIL.ImageDraw
 def Apply_logo(original_image, percent_of_side):
     width, height = original_image.size
     radius = int(percent_of_side * min(width, height)) # radius in pixels
-    
+    #you need to use the paste function to put imgages on to the mask
+    #top right corrner positioning
+    #20 transparentcy on the mask
+    #
     #start with transparent mask
     logo = PIL.Image.new('RGBA', (width, height), (127,0,127,0))
     drawing_layer = PIL.ImageDraw.Draw(Apply_logo)
-    
     # Overwrite the RGBA values with A=255.
     # The 127 for RGB values was used merely for visualizing the mask
     
@@ -19,10 +21,6 @@ def Apply_logo(original_image, percent_of_side):
     drawing_layer.polygon([(0,radius),(width,radius),
                             (width,height-radius),(0,height-radius)],
                             fill=(127,0,127,255))
-
-    #Draw four filled circles of opaqueness
-    drawing_layer.ellipse((0,0, 2*radius, 2*radius), 
-                            fill=(0,127,127,255)) #top left
                          
     # Make the new image, starting with all transparent
     result = PIL.Image.new('RGBA', original_image.size, (0,0,0,0))
@@ -45,3 +43,27 @@ def get_images(directory=None):
         except IOError:
             pass
     return image_list, file_list  
+def apply_logo_all_images(directory=None):
+    if directory == None:
+        directory = os.getcwd() # Use working directory if unspecified
+        
+    # Create a new directory 'modified'
+    new_directory = os.path.join(directory, 'modified')
+    try:
+        os.mkdir(new_directory)
+    except OSError:
+        pass # if the directory already exists, proceed  
+    
+    #load all the images
+    image_list, file_list = get_images(directory)  
+
+    #go through the images and save modified versions
+    for n in range(len(image_list)):
+        # Parse the filename
+        filename, filetype = file_list[n].split('.')
+        
+        # Round the corners with radius = 30% of short side
+        new_image = Apply_logo(image_list[n])
+        #save the altered image, suing PNG to retain transparency
+        new_image_filename = os.path.join(new_directory, filename + '.png')
+        new_image.save(new_image_filename) 
