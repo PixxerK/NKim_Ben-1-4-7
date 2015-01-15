3485fdfeab5a2abad3c56d9bae1837d3c453973c
Nathan Kim <rockwall255@gmail.com>
2015-01-14 19:00:11 -0600

    Logo V1.9
    Expected to be the last version before it's finished

--- Logo_V1.py
+++ Logo_V1.py
@@ -1,21 +1,30 @@
 import PIL
 from PIL import Image
 import os.path  
-def main(image):
-     # Open the original image
-    main = Image.open(image)
-    Picture = Image.open('watermark.png')
-    watermark = Image.new("RGBA", main.size)
-    Image.resize(Picture)
-    watermark.paste = (Picture, 0,0)
+def main():
+    directory = os.path.dirname(os.path.abspath(__file__))  
+    watermark_file = os.path.join(directory, 'watermark.png')  
+    Original_File = os.path.join(directory, 'Fire.jpg')
+    # opens the image
+    main = Image.open(Original_File)
+    secondary = Image.open(watermark_file)
     
-
+     # The Regular size
+    width, height = main.size
+    #a mask is created
+    watermark = Image.new("RGBA", main.size)
+    watermark.im.paste(watermark_file, None)
+    #pastes the watermark onto the mask
+    
+    #makes the logo white and black
     watermask = watermark.convert("L").point(lambda x: min(x, 100))
-
+    #makes the alpha
     watermark.putalpha(watermask)
- 
-    image.paste(watermark, None, watermark)
-    image.save("watermarked.jpg", "JPEG")
+    #pastes the watermark onto the original picture
+    main.paste(watermark, None, watermark)
+    #saves the picture
+    main.save("watermarked.jpg", "JPEG")
+    return main
 
 def get_images(directory=None):
 
@@ -36,20 +45,3 @@ def get_images(directory=None):
         except IOError:
             pass # do nothing with errors tying to open non-images
     return image_list, file_list
-
-def Apply_logo(directory=None):
-    if directory == None:
-        directory = os.getcwd() # Use working directory if unspecified
-
-    new_directory = os.path.join(directory, 'modified')
-    try:
-        os.mkdir(new_directory)
-    except OSError:
-        pass
-    image_list, file_list = get_images(directory)  
-
-    for n in range(len(image_list)):
-        filename, filetype = file_list[n].split('.')
-        new_image = main(image_list[n])
-        new_image_filename = os.path.join(new_directory, filename + '.png')
-        new_image.save(new_image_filename) 
