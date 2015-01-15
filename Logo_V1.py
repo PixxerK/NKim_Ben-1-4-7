7e2eeb6712cf9b98c4045f3f4eeb5c9a668b6f40
Nathan Kim <rockwall255@gmail.com>
2015-01-15 13:52:43 -0600

    Revert "Changes In code from github"
    This reverts commit 95632000c139f824f8ef4dd949e769f8b466f07c.

--- Logo_V1.py
+++ Logo_V1.py
@@ -2,7 +2,6 @@ import PIL
 from PIL import Image
 import os.path  
 <<<<<<< HEAD
-<<<<<<< HEAD
 from PIL import Image, ImageDraw
 def Logo(original_image,logo):
     width, height = original_image.size
@@ -28,23 +27,16 @@ def main():
     watermark.im.paste(watermark_file, None)
     #pastes the watermark onto the mask
 >>>>>>> origin/master
-=======
-def main(image):
-     # Open the original image
-    main = Image.open(image)
-    Picture = Image.open('watermark.png')
-    watermark = Image.new("RGBA", main.size)
-    Image.resize(Picture)
-    watermark.paste = (Picture, 0,0)
->>>>>>> parent of 3485fdf... Logo V1.9
     
-
+    #makes the logo white and black
     watermask = watermark.convert("L").point(lambda x: min(x, 100))
-
+    #makes the alpha
     watermark.putalpha(watermask)
- 
-    image.paste(watermark, None, watermark)
-    image.save("watermarked.jpg", "JPEG")
+    #pastes the watermark onto the original picture
+    main.paste(watermark, None, watermark)
+    #saves the picture
+    main.save("watermarked.jpg", "JPEG")
+    return main
 
 def get_images(directory=None):
 
@@ -65,23 +57,3 @@ def get_images(directory=None):
         except IOError:
             pass # do nothing with errors tying to open non-images
     return image_list, file_list
-<<<<<<< HEAD
-=======
-
-def Apply_logo(directory=None):
-    if directory == None:
-        directory = os.getcwd() # Use working directory if unspecified
-
-    new_directory = os.path.join(directory, 'modified')
-    try:
-        os.mkdir(new_directory)
-    except OSError:
-        pass
-    image_list, file_list = get_images(directory)  
-
-    for n in range(len(image_list)):
-        filename, filetype = file_list[n].split('.')
-        new_image = main(image_list[n])
-        new_image_filename = os.path.join(new_directory, filename + '.png')
-        new_image.save(new_image_filename) 
->>>>>>> parent of 3485fdf... Logo V1.9
